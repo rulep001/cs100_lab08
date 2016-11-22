bf9c7fdc6ba0c0238f48cd993b64fd3dad3fe2ac
Ryan Ulep <rulep001@ucr.edu>
2016-11-21 16:40:44 -0800

    Fixed declaration

--- visitor.cpp
+++ visitor.cpp
@@ -5,9 +5,11 @@
 #include "visitor.h"
 #include <string>
 
-PrintVisitor::PrintVisitor();
+PrintVisitor::PrintVisitor() {
+    
+}
 
-void PrintVisitor::ootNode() {
+void PrintVisitor::rootNode() {
 
 }
 
@@ -32,5 +34,5 @@ void PrintVisitor::opNode(Op* op) {
 }
 
 void PrintVisitor::execute() {
-    
+
 }
